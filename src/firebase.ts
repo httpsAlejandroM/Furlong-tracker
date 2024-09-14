d44ddf4f393db259c6105ee7af5f18c5c5c466a8
Alejandro Medina <ale_m@outlook.com>
2024-09-13 21:48:00 -0300

    update env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-const API_KEY = import.meta.env.VITE_API_KEY
-const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
-const DB_URL = import.meta.env.VITE_DB_URL
-const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
-const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET
-const MESSAGING_SENDER_ID = import.meta.env.VITE_STORAGE_BUCKET
-const APP_ID = import.meta.env.VITE_APP_ID
+const API_KEY = import.meta.env.API_KEY
+const AUTH_DOMAIN = import.meta.env.AUTH_DOMAIN
+const DB_URL = import.meta.env.DB_URL
+const PROJECT_ID = import.meta.env.PROJECT_ID
+const STORAGE_BUCKET = import.meta.env.STORAGE_BUCKET
+const MESSAGING_SENDER_ID = import.meta.env.STORAGE_BUCKET
+const APP_ID = import.meta.env.APP_ID
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
