f413709943aea1bce8e908e4e2b2526acfbcf18b
Alejandro Medina <ale_m@outlook.com>
2024-09-14 18:34:59 -0300

    update firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,16 +4,15 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:  "AIzaSyC6Flcf_xfWJSRmIDoM-jdEMssQBvV2F4Q",
-  authDomain:"furlong-3ffb2.firebaseapp.com",
-  databaseURL: "https://furlong-3ffb2-default-rtdb.firebaseio.com",
-  projectId: "furlong-3ffb2",
-  storageBucket: "furlong-3ffb2.appspot.com",
-  messagingSenderId:"276073528527",
-  appId: "1:276073528527:web:e926445bab5d15e5574bba"
+  apiKey:  import.meta.env.VITE_API_KEY, //"AIzaSyC6Flcf_xfWJSRmIDoM-jdEMssQBvV2F4Q",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,//"furlong-3ffb2.firebaseapp.com",
+  databaseURL: import.meta.env.VITE_DB_URL,//"https://furlong-3ffb2-default-rtdb.firebaseio.com",
+  projectId: import.meta.env.VITE_PROJECT_ID,//"furlong-3ffb2",
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,//"furlong-3ffb2.appspot.com",
+  messagingSenderId:import.meta.env.VITE_SENDER_ID,//"276073528527",
+  appId: import.meta.env.VITE_APP_ID //"1:276073528527:web:e926445bab5d15e5574bba"
 };
 
 // Initialize Firebase
